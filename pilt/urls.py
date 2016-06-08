39a070c2e9187bfdb2baab941c33002eb696d771
Marc Gonzalez Capdevila <marc@Marcs-MacBook-Pro.local>
2016-06-08 13:25:38 +0200

    Web skeleton added

--- pilt/urls.py
+++ pilt/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import include,url
 from django.contrib import admin
 from pilt.views import indexview
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('races.urls')),
     url(r'^index', indexview, name='index'),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
