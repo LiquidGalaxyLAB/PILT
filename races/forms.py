8c7347485058255f1585a3b2c52185df18d2653b
Marc Gonzalez Capdevila <marc@aules-105-050.udl.net>
2016-06-10 13:19:53 +0200

    Maps form added

--- races/forms.py
+++ races/forms.py
@@ -2,6 +2,12 @@ from django import forms
 import models
 
 class RaceForm(forms.ModelForm):
+    latitude = forms.CharField(widget=forms.TextInput(attrs={'id': 'latitudeMaps'}));
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'id': 'longitudeMaps'}));
+    localization_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'localization_nameMaps'}));
+    initial_date = forms.CharField(widget=forms.TextInput(attrs={'class':'datepicker'}))
+
+    initial_date
     class Meta:
         model = models.Race
         exclude=[]
