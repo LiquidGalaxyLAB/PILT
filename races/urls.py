fce184855108e301a560ac3b739a15a19ddb8383
Marc Gonzalez Capdevila <marcgc21@gmail.com>
2016-06-13 11:46:23 +0200

    Added serializers

--- races/urls.py
+++ races/urls.py
@@ -1,5 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import include,url
+from django.contrib import admin
+from rest_framework import routers
 from . import views
+
+
+router = routers.DefaultRouter()
+#makes sure that the API endpoints work
+router.register(r'races', views.RaceViewSet,'races')
 
 urlpatterns = [
     url(r'^air_race/$', views.airview, name='airrace'),
@@ -12,5 +19,7 @@ urlpatterns = [
     url(r'^air_race/race/edit_race/(?P<pk>\w+)/$', views.edit_race),
     url(r'^ground_race/race/delete_race/(?P<pk>\w+)/$', views.delete_race),
     url(r'^air_race/race/delete_race/(?P<pk>\w+)/$', views.delete_race),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls)),
 
 ]
