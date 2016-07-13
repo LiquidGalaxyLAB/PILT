3e51a4ab157fa55c2805dc6350b3b1826b67c88c
Marc Gonzalez Capdevila <marcgc21@gmail.com>
2016-07-13 19:26:25 +0200

    Competition + Task + Participants

--- races/models.py
+++ races/models.py
@@ -3,10 +3,9 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 from django.shortcuts import render
-
-
 # Create your models here.
 from django.http import HttpResponseRedirect
+
 
 class Competition(models.Model):
     name=models.CharField(max_length=100)
@@ -15,16 +14,21 @@ class Competition(models.Model):
 class Task(models.Model):
     name = models.CharField(max_length=100)
     imageURL = models.CharField(max_length=100)
-    file = models.FileField(upload_to='documents/%Y/%m/%d')
+    file = models.FileField(upload_to='static/competitions')
     competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
 
 
 class CompetitionTaskParticipant(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, )
+    task = models.ForeignKey(Task, on_delete=models.CASCADE )
     kmlpath = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
 
-
+class CompetitionTaskParticipantPosition(models.Model):
+    instant = models.IntegerField(default=0)
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+    height = models.CharField(max_length=100)
+    taskparticipant = models.ForeignKey(CompetitionTaskParticipant)
 
 
 class AirRace(models.Model):
